b4a2d7383ae544687da27f0149bcffba8c860670
Kylee Harward <kyleeharward21@gmail.com>
2023-11-09 15:17:02 -0700

    everything working besides "enter" for new task

--- basic-todo-app/src/app.js
+++ basic-todo-app/src/app.js
@@ -2,55 +2,43 @@
 // initial todos
 // DO NOT EDIT THIS ARRAY
 // You may add props to objects if needed.
-let todos = [
-  {
-    todoID: 0,
-    todoText: "Finish Homework",
-    todoComplete: false,
-  },
-  {
-    todoID: 1,
-    todoText: "Walk the dog",
-    todoComplete: true,
-  },
-  {
-    todoID: 2,
-    todoText: "Clean my room",
-    todoComplete: false,
-  },
-];
+let todos = []
 
 //global scopes
 const todoList = document.querySelector(".todoList");
-const newBtn = document.getElementById("newBtn");
+const newButton = document.getElementById("newBtn");
 const newTodoInput = document.getElementById("input");
 const pendingTasks = document.querySelector("#pendingTasks");
 const clearButton = document.getElementById("clearBTN");
 
-//prevent repeat todos
+//prevent repeat todos WORKS
 function removeChildren(container) {
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
 }
 
-//User can add todos
-newBtn.addEventListener("click", addNewTodo);
+//User can add todos WORKS
+
+newButton.addEventListener("click", addNewTodo);
+let newTodoID = 1
 function addNewTodo() {
   const newTodoName = document.getElementById("input").value;
   const newTodo = {
-    todoID: todos.length,
-    todoText: newTodoName,
-    todoComplete: false
+    id: newTodoID,
+    text: newTodoName,
+    complete: false
   };
   todos.push(newTodo);
   removeChildren(todoList);
   populateList(todos);
   document.getElementById("input").value = "";
+  newTodoID ++
+  console.log(newTodo);
 }
 
 //add new todo when pressing enter DOES NOT WORK YET
-newBtn.addEventListener("keyup", (event) => {
+/* newBtn.addEventListener("keyup", (event) => {
   console.log(event)
   if (event.KeyboardEvent.code==='Enter') {
     removeChildren(todoList);
@@ -65,29 +53,43 @@ newBtn.addEventListener("keyup", (event) => {
     newTodoInput.value = "";
     console.log(newTodo);
   }
-});
+}); */
 
-//User can view todos
+//User can view todos WORKS
 function populateList(todos) {
   todos.forEach((todo) => {
     let listItem = document.createElement("li")
-    listItem.innerText = todo.todoText 
-   /*  listItem.onclick = () => {
+    listItem.innerText = todo.text 
+    listItem.onclick = () => {
       toggleDone(todo.id)
-    } */
+    }
     if (todo.complete) {
       listItem.classList.add("done")
     }
+    let editButton = document.createElement("span")
+    editButton.classList.add("fa", "fa-edit", "editBtn")
+    editButton.onclick = (event) => {
+      event.stopPropagation()
+      editTodo(todo.id)
+    }
+    let trashButton = document.createElement("span")
+    trashButton.classList.add("fa", "fa-trash")
+    trashButton.onclick = (event) => {
+      event.stopPropagation()
+      deleteTodo(todo.id)
+    }
+    listItem.appendChild(trashButton)
+    listItem.appendChild(editButton)
     todoList.appendChild(listItem)
   })
   numberLeft();
 }
 
-//App shows the user number of todos left to complete
+//App shows the user number of todos left to complete WORKS
 function numberLeft() {
   let count = 0;
   for (let i in todos) {
-    if (todos[i].todoComplete === false) {
+    if (todos[i].complete === false) {
       count++;
     }
   }
@@ -95,15 +97,16 @@ function numberLeft() {
   return count;
 }
 
-//App can delete (aka clear) all done todos at once.
+//App can delete (aka clear) all done todos at once. WORKS
 clearButton.addEventListener("click", () => {
   deleteDoneTasks();
 });
 function deleteDoneTasks() {
-  todos = todos.filter((todo) => todo.todoComplete != true);
+  todos = todos.filter((todo) => todo.complete != true);
   removeChildren(todoList);
   populateList(todos);
 }
+
 //User can delete todos
 function deleteTodo(id) {
   todos = todos.filter((todo) => {
@@ -113,12 +116,17 @@ function deleteTodo(id) {
   populateList(todos);
 }
 //User can edit todos
+function getTodoById(id) {
+    return todos.find((todo) => {
+      return todo.id === id
+    })
+  }
 function editTodo(id) {
-  let newText = prompt("Edit todo:", getTodoById(id).task)
+  let newText = prompt("Edit:", getTodoById(id).text)
   if (newText !== null) {
     todos = todos.map( (todo) => {
       if (todo.id === id) {
-        todo.task = newText
+        todo.text = newText
       }
       return todo
     })
@@ -126,18 +134,18 @@ function editTodo(id) {
     populateList(todos);
   }
 }
-//User can mark todos as complete 
+
+//User can mark todos as complete WORKS
 function toggleDone(id) {
-  todos = todos.map( (todo) => {
-    if (todo.id === id) {
-      todo.complete = !todo.complete
-    }
-    return todo
-  })
-  removeChildren(todoList);
-  populateList(todos);
-}
+    todos = todos.map( (todo) => {
+      if (todo.id === id) {
+        todo.complete = !todo.complete
+      }
+      return todo
+    })
+    removeChildren(todoList);
+    populateList(todos);
+  }
 
-
-//load up todos on page load
+//load up todos on page load WORKS
 populateList(todos);
