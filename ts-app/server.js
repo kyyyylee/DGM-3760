9d8de1be79b19897385a59a264075cbde36187fe
Kylee Harward <kyleeharward21@gmail.com>
2023-12-11 17:49:20 -0700

    setting up files

---
+++ ts-app/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const hostname = "127.0.0.1"
+const app = express()
+const port = 3001
+
+app.use(express.static('basic-todo-app'))
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded ( {extended: true}))
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://${hostname}:${port}`)
+})
+
